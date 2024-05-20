bb0d71a4de06597cc3984de5bdc11f37ea471a1c
tamnh <tamnguyen231173@gmail.com>
2024-05-21 03:38:44 +0700

    feat: permission controller

--- identity-service/src/main/java/com/tamnguyen/identityService/controller/PermissionController.java
+++ identity-service/src/main/java/com/tamnguyen/identityService/controller/PermissionController.java
@@ -1,4 +1,43 @@
 package com.tamnguyen.identityService.controller;
 
+import com.tamnguyen.identityService.dto.request.PermissionRequest;
+import com.tamnguyen.identityService.dto.response.ApiResponse;
+import com.tamnguyen.identityService.dto.response.PermissionResponse;
+import com.tamnguyen.identityService.service.PermissionService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/permissions")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
 public class PermissionController {
+    PermissionService permissionService;
+
+    @PostMapping("/create")
+    ApiResponse<PermissionResponse> createPermission(@RequestBody PermissionRequest request) {
+        return ApiResponse.<PermissionResponse>builder()
+                .result(permissionService.create(request))
+                .build();
+    }
+
+    @GetMapping("/")
+    ApiResponse<List<PermissionResponse>> getAllPermissions() {
+        return ApiResponse.<List<PermissionResponse>>builder()
+                .result(permissionService.getAll())
+                .build();
+    }
+
+    @DeleteMapping("/{permission}")
+    ApiResponse<Void> deletePermission(@PathVariable String permission) {
+       permissionService.delete(permission);
+       return ApiResponse.<Void>builder().build();
+    }
 }
--- identity-service/src/main/java/com/tamnguyen/identityService/service/PermissionService.java
+++ identity-service/src/main/java/com/tamnguyen/identityService/service/PermissionService.java
@@ -8,6 +8,16 @@ import com.tamnguyen.identityService.repository.PermissionRepository;
 
 import java.util.List;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class PermissionService {
     PermissionRepository permissionRepository;
     PermissionMapper permissionMapper;
