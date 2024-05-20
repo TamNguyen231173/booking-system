da4caa3ed0933463b3d38ca5d0b0f42f4a1d904f
tamnh <tamnguyen231173@gmail.com>
2024-05-21 03:07:01 +0700

    feat: role controller

--- identity-service/src/main/java/com/tamnguyen/identityService/controller/RoleController.java
+++ identity-service/src/main/java/com/tamnguyen/identityService/controller/RoleController.java
@@ -1,4 +1,43 @@
 package com.tamnguyen.identityService.controller;
 
+import java.util.List;
+
+import com.tamnguyen.identityService.dto.request.RoleRequest;
+import com.tamnguyen.identityService.dto.response.ApiResponse;
+import com.tamnguyen.identityService.dto.response.RoleResponse;
+import com.tamnguyen.identityService.service.RoleService;
+import org.springframework.web.bind.annotation.*;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/roles")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
 public class RoleController {
+    RoleService roleService;
+
+    @PostMapping("/create")
+    ApiResponse<RoleResponse> create(@RequestBody RoleRequest request) {
+        return ApiResponse.<RoleResponse>builder()
+                .result(roleService.create(request))
+                .build();
+    }
+
+    @GetMapping("/")
+    ApiResponse<List<RoleResponse>> getAll() {
+        return ApiResponse.<List<RoleResponse>>builder()
+                .result(roleService.getAll())
+                .build();
+    }
+
+    @DeleteMapping("/{role}")
+    ApiResponse<Void> delete(@PathVariable String role) {
+        roleService.delete(role);
+        return ApiResponse.<Void>builder().build();
+    }
 }
