12fab58087201060fc06cc30cf2cf05368836ea4
tamnh <tamnguyen231173@gmail.com>
2024-05-23 04:32:02 +0700

    feat: add test properties

--- identity-service/src/test/java/com/tamnguyen/identityService/controller/UserControllerTest.java
+++ identity-service/src/test/java/com/tamnguyen/identityService/controller/UserControllerTest.java
@@ -15,8 +15,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcResultMatchersDsl;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -25,6 +25,7 @@ import java.time.LocalDate;
 @Slf4j
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource("/test.properties")
 public class UserControllerTest {
 
     @Autowired
--- identity-service/src/test/java/com/tamnguyen/identityService/service/UserServiceTest.java
+++ identity-service/src/test/java/com/tamnguyen/identityService/service/UserServiceTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDate;
 import java.util.Optional;
@@ -23,7 +24,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
-//@TestPropertySource("/test.properties")
+@TestPropertySource("/test.properties")
 public class UserServiceTest {
     @Autowired
     UserService userService;
