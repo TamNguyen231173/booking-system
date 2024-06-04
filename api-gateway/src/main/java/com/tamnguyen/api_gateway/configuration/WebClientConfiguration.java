4b4902852ade1e5a0e4b75c4fb489b8895ad0d98
tamnh <tamnguyen231173@gmail.com>
2024-06-04 19:41:41 +0700

    feat: config cors for gateway

--- api-gateway/src/main/java/com/tamnguyen/api_gateway/configuration/WebClientConfiguration.java
+++ api-gateway/src/main/java/com/tamnguyen/api_gateway/configuration/WebClientConfiguration.java
@@ -3,9 +3,14 @@ package com.tamnguyen.api_gateway.configuration;
 import com.tamnguyen.api_gateway.repository.IdentityClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.List;
 
 @Configuration
 public class WebClientConfiguration {
@@ -14,6 +19,19 @@ public class WebClientConfiguration {
         return WebClient.builder()
                 .baseUrl("http://localhost:8080/identity")
                 .build();
+    }
+
+    @Bean
+    CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsWebFilter(source);
     }
 
     @Bean
